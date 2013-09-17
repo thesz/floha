070f0d442654375f3c4f1d169bbba95f8910be91
serguey.zefirov <serguey.zefirov@parsci.com>
2013-09-18 00:59:02 +0400

    Current progress.

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ floha
 
 Data flow hardware description language
 
-The name Floha stands for "data FLOw HArdware", or "data FLOw HAskell", whatever suits you better.
+The name Floha stands for "data FLOw HArdware", or "data FLOw HAskell", whatever suits you better. There's also
+a river Floha (o-umlaut) in Germany, so one more association with "flow of something".
 
 It is similar to CAPH data flow hardware language: http://wwwlasmea.univ-bpclermont.fr/Personnel/Jocelyn.Serot/caph.html
 
-I hope to fix some deficiencies in CAPH.
+I hope to fix some deficiencies in CAPH. Namely:
+ * CAPH does not support multiclock networks, it also does not provide means to separate or speed up networks with different types of FIFOs.
+ * CAPH does not properly support memory blocks in FPGA. Most such blocks returns results 
--- src/Language/Floha.hs
+++ src/Language/Floha.hs
@@ -5,6 +5,8 @@
 --
 -- Copyright (C) 2013 Serguey Zefirov
 
+{-# LANGUAGE GADTs, TypeOperators, TypeFamilies #-}
+
 module Language.Floha where
 
 import Control.Monad
@@ -12,4 +14,28 @@ import Control.Monad.State
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+
+infixr 5 :.
+data a :. b = a :. b
+data Nil = Nil
+
+data VarID = VarID (Maybe String) Int
+	deriving (Eq, Ord, Show)
+
+-- |Floha expression structure.
+data FE a where
+	FConst :: a -> FE a
+	FVar :: VarID -> FE a
+
+-- |Lift HList to the HList of Floha expressions.
+type family LiftFE ts
+type instance LiftFE Nil = Nil
+type instance LiftFE (t :. ts) = FE t :. LiftFE ts
+
+-- |Floha actor.
+data Actor ins outs where
+	-- |Actor is either real actor - a state machine.
+	Actor :: LiftFE ins -> LiftFE outs -> Actor ins outs
+	-- |Or actor is a network of connections between actors.
+	Network :: Actor ins outs
 
